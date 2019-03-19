a0bc1b2a7e04c6787250e47f3dd7a91ec16b0594
RicWg <flyingrichard@yahoo.com>
2019-03-19 12:13:04 -0700

    Create mab_probmatching.R

---
+++ mab_probmatching.R
@@ -0,0 +1,113 @@
+# CS234 RL 2019
+# Multi-armed bandit(MAB) policy: Probability matching
+# Policy: 
+#   adjust probability with reward to match test takers'
+#   2 sets of prior probability 
+
+mypackages = c("mirt", "rlist", "gtools")   
+tmp = setdiff(mypackages, rownames(installed.packages()))  
+if (length(tmp) > 0) install.packages(tmp)
+lapply(mypackages, require, character.only = TRUE)
+
+require(ggplot2)
+library(dplyr)
+
+setwd("~/cs234_RL_Stanford/")
+pronability = read.csv('rl_score_accumulated.csv', header=TRUE, sep=",")
+item_diff = read.csv('item_diff.csv', header=TRUE, sep=",")
+
+# Load IRT model from env
+load('rl_irt.RData')  
+
+p_ability = data.frame()
+window_size = 8
+num_episod = 1000
+rew = 0.0
+epislon = 0.2
+num_cycle = 3
+
+for (j in 1:dim(dt)[1]) {
+#for (j in 1:300) {
+  callsc = dt[j,]
+  
+  # loop through column withnot NA
+  idx = is.na(callsc)
+  pos = which(idx %in% c(FALSE))
+  print(j)
+  
+  # generate sequences of pos
+  for (k in 1:num_episod){
+    sc = array(c(NA), dim = dim(dt)[2])    
+    seq_pos = order(-item_diff[colnames(callsc[pos]),])
+    seq_pos = pos[seq_pos]
+    for (c in r:num_cycle) {
+      for (i in 1:length(pos)/num_cycle) {
+        i = num_cycle*(length(pos)/num_cycle) + i
+        #sc = array(c(NA), dim = dim(idx)[2])
+        p = seq_pos[i]
+
+        # exploitation/exploration by reward
+        ran = runif(1,0,1)
+        if (rew > 0){
+          if (ran > epsilon) {
+              sc[p] = callsc[p+1]
+            }
+        }
+        else {
+          sc[p] = callsc[p]
+        }
+
+        # reset sc[i - window_size] to NA, only use scores in window to calculate
+        if (i > window_size) {
+          sc[seq_pos[i-window_size]] = NA
+        }
+
+        respsc = list()
+        step = fscores(mod_irt, method='MAP', response.pattern = sc)
+
+        respsc = data.frame(
+          call = pron_call[j,]$call,
+          item = colnames(callsc[p]),
+          items_score = as.numeric(step[length(step)-1]),
+          item_scorestd = as.numeric(step[length(step)]) )
+
+        p_ability = bind_rows(p_ability,respsc)
+        rew = p_ability[length[p_ability]]$item_scorestd - p_ability[length[p_ability]-1]$item_scorestd
+        }
+    }
+  }
+}
+#print(p_ability)
+
+# check mean
+dtm = matrix(p_ability[,'items_score'], nrow=25)
+dtm_mean = melt(data.frame(rowSums(dtm))/n)
+p = ggplot() + 
+  geom_line(data=melt(dtm), aes(x = Var1, y = value, group = Var2)) +
+  xlab('Items') + ylab('Ability') +
+  geom_line(aes(x = c(1:25), y = dtm_mean['value'], colour='red')) + geom_line(size=2)
+print(p)
+
+# check std
+dtm_std = melt(matrix(p_ability[,'item_scorestd'], nrow=25))
+p = ggplot() + 
+  geom_line(data=dtm_std, aes(x = Var1, y = value, group = Var2)) +
+  xlab('Items') + ylab('Score by window') 
+print(p)
+
+# check reward
+dtm_reward = matrix(p_ability[,'item_scorestd'], nrow=25)
+for (j in 2:25){
+  dtm_reward[j,] = dtm_reward[1,] - dtm_reward[j,] 
+}
+dtm_reward[1,] = 0.0
+
+dtrew_std = melt(data.frame(rowSums(dtm_reward))/dim(dtm_reward)[2])
+p = ggplot() + 
+  geom_line(data=melt(dtm_reward), aes(x = X1, y = value, group = X2)) +
+  xlab('Items') + ylab('Return') +
+  geom_line(aes(x = c(1:25), y = dtrew_std['value'], colour='red')) 
+print(p)
+
+# output to csv for pyplot
+write.csv(dtm_reward, 'mab_multistage.csv')
